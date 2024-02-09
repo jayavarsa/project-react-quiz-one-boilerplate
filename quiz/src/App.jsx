eeab6eef215e5f1a816bca12f63a715001aeef16
jayavarsan <jayavarsan.r@kalvium.community>
2024-02-09 22:24:10 +0530

    n

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home'
-// import Quiz from './components/Quiz';
-// import Result from './components/Result';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
 
 function App() {
   return (
     <div className="App">
       <Home/>
-      {/* <Quiz/> */}
-      {/* <Result /> */}
+      <Quiz/> 
+      <Result /> 
     </div>
   );
 }
